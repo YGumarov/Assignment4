eb6f4b323c5ed374346369585e273eda6249dc2e
YGumarov <yerassylgumarov@gmail.com>
2023-05-12 10:11:46 +0600

    mda

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class BST<K, V> where K : IComparable<K>
+{
+    private Node root;
+    private class Node
+    {
+        public K key;
+        public V value;
+        public Node left, right;
+        public int size;
+
+        public Node(K key, V value)
+        {
+            this.key = key;
+            this.value = value;
+            this.size = 1;
+        }
+    }
+    public void put(K key, V val) {…}
+    public V get(K key) {…}
+    public void delete(K key){…}
+    public Iterable<K> iterator() {…}
+}
